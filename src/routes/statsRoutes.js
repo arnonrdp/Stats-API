b2bc43ce3f71e95c16253e22a88ec998aeb67def
Arnon R <arnonrdp@gmail.com>
2023-11-18 09:41:42 -0300

    Enable GET request for stats

--- src/routes/statsRoutes.js
+++ src/routes/statsRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const statsController = require('../controllers/statsController')
 
 router.post('/stats', statsController.create)
-// router.get('/stats', statsController.read)
+router.get('/stats', statsController.read)
 
 module.exports = router
