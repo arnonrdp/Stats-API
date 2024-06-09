16c97e2d8e6326fa4d14af4bbc0cbc3da0ff6e09
Marc Lupanciuc <marclupanc@gmail.com>
2024-06-09 17:47:06 +0300

    fix: added express json for one specific route

--- src/routes/users/userRoutes.js
+++ src/routes/users/userRoutes.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/users')
 
+const app = express()
+
 router.post('/users', userController.addUser)
-router.post('/add-all-users', userController.addAllUsersToDB)
+router.post('/add-all-users', app.use(express.json()), userController.addAllUsersToDB)
 router.get('/users', userController.getAllUsers)
 router.get('/user', userController.getUserById)
 router.patch('/users', userController.updateUser)
