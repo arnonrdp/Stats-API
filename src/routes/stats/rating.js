8f99af388c95bd910e7cfb52b5a4306cf2ea40c3
Marc Lupanciuc <marclupanc@gmail.com>
2024-06-13 23:14:32 +0300

    fix: removing express.json usage

--- src/routes/stats/rating.js
+++ src/routes/stats/rating.js
@@ -1,9 +1,7 @@
 const { Router } = require('express')
 const ratingController = require('../../controllers/stats/postRating')
-const express = require('express')
 const router = Router()
 
-const app = express()
-router.get('/post-rating', app.use(express.json()), ratingController.getPostRating)
+router.get('/post-rating', ratingController.getPostRating)
 
 module.exports = router
--- src/routes/stats/statistics.js
+++ src/routes/stats/statistics.js
@@ -2,10 +2,9 @@ const express = require('express')
 const router = express.Router()
 const statsController = require('../../controllers/stats/statistics')
 
-const app = express()
 router.post('/stats', statsController.create)
 router.get('/stats', statsController.getAllStats)
-router.get('/stats/article', app.use(express.json()), statsController.getArticleStats)
+router.get('/stats/article', statsController.getArticleStats)
 router.get('/stats/users-locations', statsController.getUsersLocations)
 router.get('/stats/metrics', statsController.getMetricsByCountry)
 
