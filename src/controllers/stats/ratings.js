9785b042bd5c673ff97b209eef4fb5f4fed42f2f
Marc Lupanciuc <marclupanc@gmail.com>
2024-07-27 00:00:27 +0300

    Logging headers to see where the ip is hidden

--- src/controllers/stats/ratings.js
+++ src/controllers/stats/ratings.js
@@ -320,7 +320,7 @@ const trace = (req, res) => {
   const ipAddress = req.headers['X-Envoy-External-Address'] || req.connection.remoteAddress
   // Clean the IP address if it includes IPv6 prefix
   const cleanedIp = ipAddress.replace(/^::ffff:/, '')
-  console.log(ipAddress)
+  console.log(req.headers)
   try {
     const location = ip2location.getCountryShort(cleanedIp)
     res.status(200).json({
